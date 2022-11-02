a3c6d6704d4600b34f896ebb7fe6890e30e820bf
Nate <nathanael.paul.lewis@gmail.com>
2022-11-01 19:36:09 -0500

    tried to get mouse click to work unsuccessful

--- test.py
+++ test.py
@@ -7,50 +7,45 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("TIC TAC TOE")
 
 BG_COLOR = (32, 32, 32,)
-
 FPS = 60
+CLOCK = pygame.time.Clock()
 VEL = 5
 CHARACTER_WIDTH, CHARACTER_HEIGHT = 256, 256
+
+
 
 X9 = pygame.image.load(os.path.join('assets', 'tic_tac_toe_x9.png'))
 X8 = pygame.image.load(os.path.join('assets', 'tic_tac_toe_x8.png'))
 GAME_BOARD = pygame.image.load(os.path.join('assets', 'tic_tac_toe_game_board.png'))
 
-
 def draw_window():
     WIN.fill(BG_COLOR)
     WIN.blit(GAME_BOARD, (0, 0))
     if pygame.MOUSEBUTTONDOWN:
-        mouse_button_pressed = pygame.mouse.get_pressed()
-        if mouse_button_pressed[0]:
-            WIN.blit(X8, (0, 0))
+        mouse_click = pygame.mouse.get_pressed()
+        if mouse_click[0]:
+            WIN.blit(X9, (0, 0))
+            print('left click')
     pygame.display.update()
-
 
 def x_character():
-    if pygame.MOUSEBUTTONDOWN:
-        mouse_button_pressed = pygame.mouse.get_pressed()
-        if mouse_button_pressed[0]:
-            print("left is being pressed")
-            WIN.blit(X9, (0, 0))
-    pygame.display.update()
-
+    mouse_click = pygame.mouse.get_pressed()
+    if mouse_click[0]:
+        WIN.blit(X8, (0, 0))
+        print('left click')
+        pygame.display.update()
 
 def main():
-    clock = pygame.time.Clock()
     run = True
     while run:
-        clock.tick(FPS)
+        CLOCK.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 run = False
-
-            x_character()
+                quit()
         draw_window()
-
-    pygame.quit()
-
-
+        x_character()
+        pygame.display.update()
 if __name__ == "__main__":
     main()
-
